77cba325a1ddb520d944bfa666a3e78e9ca42bd9
Rob <robrichardsdpt@gmail.com>
2021-01-01 09:06:04 -0500

    updated user schema

---
+++ app/models/user.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true
+  },
+  lastName: {
+    type: String,
+    required: true
+  },
+  role: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  hashedPassword: {
+    type: String,
+    required: true
+  },
+  token: String
+}, {
+  timestamps: true,
+  toObject: {
+    // remove `hashedPassword` field when we call `.toObject`
+    transform: (_doc, user) => {
+      delete user.hashedPassword
+      return user
+    }
+  }
+})
+
+module.exports = mongoose.model('User', userSchema)
